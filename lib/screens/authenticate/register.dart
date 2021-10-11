afbc169ce6a032652683f2f4679b924c81878afb
Hizkia Luke <contact.hizkia@gmail.com>
2021-10-11 10:14:27 +0700

    Add loader
    Add loader while waiting

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sigmoid/services/auth.dart';
+import 'package:sigmoid/shared/constants.dart';
+import 'package:sigmoid/shared/loading.dart';
 
 class Register extends StatefulWidget {
   // const Register({Key? key}) : super(key: key);
@@ -14,6 +16,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _authService = AuthService();
   final _formkey = GlobalKey<FormState>();
+  bool showLoading = false;
 
   // text field state
   String email = '';
@@ -22,70 +25,81 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign up to Sigmoid'),
-        actions: <Widget>[
-          TextButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Sign In'))
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-          child: Form(
-            key: _formkey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20),
-                TextFormField(
-                  validator: (val) => val!.isEmpty ? 'Enter an email' : null,
-                  onChanged: (val) {
-                    setState(() => email = val.trim());
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  obscureText: true,
-                  validator: (val) => val!.length < 6
-                      ? 'Password must be longer than 6 chars'
-                      : null,
-                  onChanged: (val) {
-                    setState(() => password = val.trim());
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  child: Text('Sign Up'),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.pink[400]),
-                      textStyle: MaterialStateProperty.all(
-                          TextStyle(color: Colors.white))),
-                  onPressed: () async {
-                    if (_formkey.currentState!.validate()) {
-                      dynamic result =
-                          await _authService.registerWithEmail(email, password);
-                      if (result == null) {
-                        setState(() => errorMsg = 'please input a valid email');
-                      }
-                    }
-                  },
-                ),
-                SizedBox(height: 20),
-                Text(
-                  errorMsg,
-                  style: TextStyle(color: Colors.red),
-                )
+    return showLoading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[50],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign up to Sigmoid'),
+              actions: <Widget>[
+                TextButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
+                    },
+                    icon: Icon(Icons.person),
+                    label: Text('Sign In'))
               ],
             ),
-          )),
-    );
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) =>
+                            val!.isEmpty ? 'Enter an email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val.trim());
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        obscureText: true,
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        validator: (val) => val!.length < 6
+                            ? 'Password must be longer than 6 chars'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => password = val.trim());
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        child: Text('Sign Up'),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.pink[400]),
+                            textStyle: MaterialStateProperty.all(
+                                TextStyle(color: Colors.white))),
+                        onPressed: () async {
+                          if (_formkey.currentState!.validate()) {
+                            setState(() => showLoading = true);
+                            dynamic result = await _authService
+                                .registerWithEmail(email, password);
+                            if (result == null) {
+                              setState(() {
+                                errorMsg = 'Please input a valid email';
+                                showLoading = false;
+                              });
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        errorMsg,
+                        style: TextStyle(color: Colors.red),
+                      )
+                    ],
+                  ),
+                )),
+          );
   }
 }
--- lib/screens/authenticate/signin.dart
+++ lib/screens/authenticate/signin.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sigmoid/services/auth.dart';
+import 'package:sigmoid/shared/constants.dart';
+import 'package:sigmoid/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   // const SignIn({Key? key}) : super(key: key);
@@ -14,6 +16,7 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _authService = AuthService();
   final _formkey = GlobalKey<FormState>();
+  bool showLoading = false;
 
   // text field state
   String email = '';
@@ -22,70 +25,81 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign in to Sigmoid'),
-        actions: <Widget>[
-          TextButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Sign Up'))
-        ],
-      ),
-      body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-          child: Form(
-            key: _formkey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20),
-                TextFormField(
-                  validator: (val) => val!.isEmpty ? 'Enter an email' : null,
-                  onChanged: (val) {
-                    setState(() => email = val.trim());
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  obscureText: true,
-                  validator: (val) => val!.length < 6
-                      ? 'Password must be longer than 6 chars'
-                      : null,
-                  onChanged: (val) {
-                    setState(() => password = val.trim());
-                  },
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  child: Text('Sign In'),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.pink[400]),
-                      textStyle: MaterialStateProperty.all(
-                          TextStyle(color: Colors.white))),
-                  onPressed: () async {
-                    if (_formkey.currentState!.validate()) {
-                      dynamic result =
-                          await _authService.signInWithEmail(email, password);
-                      if (result == null) {
-                        setState(() => errorMsg = 'Credentials not found');
-                      }
-                    }
-                  },
-                ),
-                SizedBox(height: 20),
-                Text(
-                  errorMsg,
-                  style: TextStyle(color: Colors.red),
-                )
+    return showLoading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[50],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Sign in to Sigmoid'),
+              actions: <Widget>[
+                TextButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
+                    },
+                    icon: Icon(Icons.person),
+                    label: Text('Sign Up'))
               ],
             ),
-          )),
-    );
+            body: Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20),
+                      TextFormField(
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Email'),
+                        validator: (val) =>
+                            val!.isEmpty ? 'Enter an email' : null,
+                        onChanged: (val) {
+                          setState(() => email = val.trim());
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        obscureText: true,
+                        decoration:
+                            textInputDecoration.copyWith(hintText: 'Password'),
+                        validator: (val) => val!.length < 6
+                            ? 'Password must be longer than 6 chars'
+                            : null,
+                        onChanged: (val) {
+                          setState(() => password = val.trim());
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        child: Text('Sign In'),
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.pink[400]),
+                            textStyle: MaterialStateProperty.all(
+                                TextStyle(color: Colors.white))),
+                        onPressed: () async {
+                          if (_formkey.currentState!.validate()) {
+                            setState(() => showLoading = true);
+                            dynamic result = await _authService.signInWithEmail(
+                                email, password);
+                            if (result == null) {
+                              setState(() {
+                                errorMsg = 'Credentials not found';
+                                showLoading = false;
+                              });
+                            }
+                          }
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        errorMsg,
+                        style: TextStyle(color: Colors.red),
+                      )
+                    ],
+                  ),
+                )),
+          );
   }
 }
